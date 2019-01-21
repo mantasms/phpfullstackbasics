5e6d7a719d4b6b7754fd43ef9d3389d9d9b9775a
mantasms <m.sakaitis@gmail.com>
2019-01-21 11:37:02 +0200

    html teksto komentarai

--- index.php
+++ index.php
@@ -3,10 +3,19 @@
         <title>Komentarai</title>
     </head>
     <body>
+        <!-- pirmo div'o paaiskinimas labai ilgas
+        bla bla
+        -->
         <div>
             <h1>As pradedu</h1>
             <h2>O as papildau</h2>
         </div>
+        <!--
+        <div>
+            <h1>As pradedu</h1>
+            <h2>O as papildau</h2>
+        </div>
+        -->
             <p>
                 As esu ta pana, kuri kalba ir kalba, ir kalba, ir kalba...
             </p>
