47f0499c2c02f581e6ce70ecd816ebb67d4c60d1
mantasms <m.sakaitis@gmail.com>
2019-01-21 11:24:47 +0200

    html teksto paruosimas komentarams

--- index.php
+++ index.php
@@ -4,11 +4,11 @@
     </head>
     <body>
         <div>
-        <h1>As pradedu</h1>
-        <h2>O as papildau</h2>
+            <h1>As pradedu</h1>
+            <h2>O as papildau</h2>
         </div>
-        <p>
-            As esu tas pana, kuri kalba ir kalba, ir kalba, ir kalba...
-        </p>
+            <p>
+                As esu ta pana, kuri kalba ir kalba, ir kalba, ir kalba...
+            </p>
     </body>
 </html>
