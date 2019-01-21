499802b395f5bb892fe3bbd80e83120c1f128267
mantasms <m.sakaitis@gmail.com>
2019-01-21 11:41:58 +0200

    html teksto pataisyti komentarai

--- index.php
+++ index.php
@@ -3,9 +3,7 @@
         <title>Komentarai</title>
     </head>
     <body>
-        <!-- pirmo div'o paaiskinimas labai ilgas
-        bla bla
-        -->
+        <!--pirmo div'o paaiskinimas labai ilgas-->
         <div>
             <h1>As pradedu</h1>
             <h2>O as papildau</h2>
@@ -14,10 +12,10 @@
         <div>
             <h1>As pradedu</h1>
             <h2>O as papildau</h2>
-        </div>
-        -->
             <p>
                 As esu ta pana, kuri kalba ir kalba, ir kalba, ir kalba...
             </p>
+        </div>
+        -->
     </body>
 </html>
