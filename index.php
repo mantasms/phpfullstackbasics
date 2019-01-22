4fae35d0245c0c02642e60e8e16667492e8d2d76
mantasms <m.sakaitis@gmail.com>
2019-01-22 12:02:12 +0200

    kodo taisymas

--- index.php
+++ index.php
@@ -1,19 +1,16 @@
 <?php
-$sudas1 = rand(1,100);
-$sudas2 = rand(101,200);
-$sudas3 = rand(201,300);
-$sudas4 = rand(301,400);
+$suma = rand(1,100);
+$suma1 = rand(101,200);
+$suma2 = rand(201,300);
 ?>
 <html>
     <head>
         <title>Manto PHP pamokos</title>
     </head>
     <body>
-        <div>
-            <h1>Skolos skaiciuokle</h1>
-            <h2>Jei paemiai <?php print $sudas1; ?> jievru</h2>
-            <h3>Su dviem kabanciais grazinsi <?php print $sudas3; ?></h3>
-            <h3>Su vienu kabanciu grazinsi <?php print $sudas2; ?></h3>
-        </div>       
+        <h1>Skolos skaiciuokle</h1>
+        <h2>Jei paemiai <?php print $suma; ?> jievru</h2>
+        <h3>Su dviem kabanciais grazinsi <?php print $suma1; ?></h3>
+        <h3>Su vienu kabanciu grazinsi <?php print $suma2; ?></h3>
     </body>
 </html>
