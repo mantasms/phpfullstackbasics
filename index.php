4a12ba2de9143e2a7cbf6538b6960f242dc773e7
mantasms <m.sakaitis@gmail.com>
2019-01-22 13:51:55 +0200

    saulute

--- index.php
+++ index.php
@@ -1,16 +1,12 @@
 <?php
-$suma = rand(1,100);
-$suma1 = rand(101,200);
-$suma2 = rand(201,300);
+$sunny = rand(0,1);
 ?>
 <html>
     <head>
-        <title>Manto PHP pamokos</title>
+        <link rel="stylesheet" type="text/css" href="/css/main.css">
+        <title>Uzduotis 5</title>
     </head>
-    <body>
-        <h1>Skolos skaiciuokle</h1>
-        <h2>Jei paemiai <?php print $suma; ?> jievru</h2>
-        <h3>Su dviem kabanciais grazinsi <?php print $suma1; ?></h3>
-        <h3>Su vienu kabanciu grazinsi <?php print $suma2; ?></h3>
+    <body>    
+        <div class="<?php print 'sauleta' . $sunny; ?>"></div>
     </body>
 </html>
