28b0773dac82766bde97901ba3e3ea5238beae0c
mantasms <m.sakaitis@gmail.com>
2019-01-22 11:51:32 +0200

    kodo taisymas

--- index.php
+++ index.php
@@ -11,9 +11,9 @@ $sudas4 = rand(301,400);
     <body>
         <div>
             <h1>Skolos skaiciuokle</h1>
-            <h3>Jei paemiai <?php print $sudas1;?> jievru</h3>
-            <h3>Su dviem kabanciais grazinsi <?php print $sudas3;?></h3>
-            <h3>Su vienu kabanciu grazinsi <?php print $sudas2;?></h3>
+            <h3>Jei paemiai <?php print $sudas1; ?> jievru</h3>
+            <h3>Su dviem kabanciais grazinsi <?php print $sudas3; ?></h3>
+            <h3>Su vienu kabanciu grazinsi <?php print $sudas2; ?></h3>
          </div>       
     </body>
 </html>
