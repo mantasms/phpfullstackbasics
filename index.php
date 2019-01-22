d40f0fc2f2636072e504042a1cf693143a61aff9
mantasms <m.sakaitis@gmail.com>
2019-01-22 11:18:08 +0200

    html intro uzduotis 1

--- index.php
+++ index.php
@@ -1,24 +1,23 @@
+<?php
+$string = 'stringai';
+$integer = 20;
+$float = 9.99;
+$boolean = true;
+$null = null;
+?>
 <html>
     <head>
-        <title>Komentarai</title>
+        <link rel="stylesheet" type="text/css" href="main.css">
+        <title>PHP ...></title>
     </head>
     <body>
-        <?php
-        /*
         <div>
-            <h1>As pradedu</h1>
-            <h2>O as papildau</h2>
+            <h1></h1>
+            <h2><?php print $string ?></h2>
+            <h2><?php print $integer ?></h2>
+            <h2><?php print $float ?></h2>
+            <h2><?php print $boolean ?></h2>
+            <h2><?php print $null ?></h2>       
         </div>       
-        */
-        ?>
-        <!--
-        <div>
-            <h1>As pradedu</h1>
-            <h2>O as papildau</h2>
-            <p>
-                As esu ta pana, kuri kalba ir kalba, ir kalba, ir kalba...
-            </p>
-        </div>
-        -->
     </body>
 </html>
