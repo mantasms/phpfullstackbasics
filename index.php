22b4a391b2968c6c12d4b5a9c44586322b2bdd1c
mantasms <m.sakaitis@gmail.com>
2019-01-22 11:51:20 +0200

    kodo taisymas

--- index.php
+++ index.php
@@ -1,23 +1,19 @@
 <?php
-$string = 'stringai';
-$integer = 20;
-$float = 9.99;
-$boolean = true;
-$null = null;
+$sudas1 = rand(1,100);
+$sudas2 = rand(101,200);
+$sudas3 = rand(201,300);
+$sudas4 = rand(301,400);
 ?>
 <html>
     <head>
-        <link rel="stylesheet" type="text/css" href="main.css">
-        <title>PHP ...></title>
+        <title>Manto PHP pamokos></title>
     </head>
     <body>
         <div>
-            <h1></h1>
-            <h2><?php print 'string ' . $string ?></h2>
-            <h2><?php print 'integer ' . $integer ?></h2>
-            <h2><?php print 'float ' . $float ?></h2>
-            <h2><?php print 'boolean ' . $boolean ?></h2>
-            <h2><?php print 'null ' . $null ?></h2>       
-        </div>       
+            <h1>Skolos skaiciuokle</h1>
+            <h3>Jei paemiai <?php print $sudas1;?> jievru</h3>
+            <h3>Su dviem kabanciais grazinsi <?php print $sudas3;?></h3>
+            <h3>Su vienu kabanciu grazinsi <?php print $sudas2;?></h3>
+         </div>       
     </body>
 </html>
