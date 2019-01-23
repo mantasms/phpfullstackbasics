c507d217093421ce982045524933344e293c5208
mantasms <m.sakaitis@gmail.com>
2019-01-23 14:04:58 +0200

    cizu islaidu kalkuliatorius

--- index.php
+++ index.php
@@ -7,10 +7,12 @@ $pakelio_kaina = 3.30;
 $viso_vnt = 0;
 
 for ($x = 1; $x <= $days; $x++) {
-    $cizos_mon_thu = rand(7, 10);
     $date = date('N', strtotime("+$x day"));
-    $viso_vnt += $cizos_mon_thu;
-    print $date . ' dieni ' . $cizos_mon_thu . ' cizu reiskia ' . $viso_vnt . ' is viso <br>';
+    if ($date <=4) {
+        $cizos_mon_thu = rand(7, 10);
+        $viso_vnt += $cizos_mon_thu;
+        print $date . ' dieni ' . $cizos_mon_thu . ' cizu reiskia ' . $viso_vnt . ' is viso <br>';
+    }  
 }
 ?>
 <html>
