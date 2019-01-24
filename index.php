a310ab17e7cc0682ba9a0043b3aa76ce51c25cb1
mantasms <m.sakaitis@gmail.com>
2019-01-24 13:43:24 +0200

    afygenas arrayjus

--- index.php
+++ index.php
@@ -1,28 +1,28 @@
 <?php
 
 $zmones = [
-        'Petras Pizdzius' => [
-            'daiktai' => [
-                'telefonas' => [
-                    'statusas' => 'prapistas'
-                    ]
-                ]
-            ],
-        'Tomas Ablomas' => [
-            'daiktai' => [
-                'buljonas' => [
-                    'statusas' => 'naudojamas'
-                    ]
-                ]
-            ],
-        'Ana Shitkova' => [
-            'daiktai' =>[
-                'rulonas' => [
-                    'statusas' =>'pasibaiges'
-                    ]
-                ]
+    'Petras Pizdzius' => [
+        'daiktai' => [
+            'telefonas' => [
+                'statusas' => 'prapistas'
             ]
-        ];
+        ]
+    ],
+    'Tomas Ablomas' => [
+        'daiktai' => [
+            'buljonas' => [
+                'statusas' => 'naudojamas'
+            ]
+        ]
+    ],
+    'Ana Shitkova' => [
+        'daiktai' => [
+            'rulonas' => [
+                'statusas' => 'pasibaiges'
+            ]
+        ]
+    ]
+];
 
 var_dump($zmones);
 ?>
