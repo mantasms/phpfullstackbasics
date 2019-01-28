6da03b9256a12bab84804bdb0aee3a26aeda5265
mantasms <m.sakaitis@gmail.com>
2019-01-28 12:33:51 +0200

    foreach ciklas

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
 $mano_atmintis = ['Penktadienis', 'Paskaitos', 'Zmona', 'Parduotuve', 'Mokykla', 'Namai', 'Sriuba', 'Alus', 'Pica', 'Alus', 'Vynas', 'Kosmosas'];
+$draugo_atmintis = ['Penktadienis', 'Paskaitos', 'Alus', 'Alus', 'Degtine', 'Namai', 'Bliudas'];
+
 $count = count($mano_atmintis);
 $rand = rand(1, $count);
 $key = $rand - 1;
@@ -11,10 +13,18 @@ $key = $rand - 1;
     </head>
     <body>
         <h1>WTF?</h1>
-        <h2>mano atmintis</h2>
+        <h2>Mano atmintis</h2>
         <h3>Flashback <?php print $rand . ': ' . $mano_atmintis[$key]; ?> </h3>
         <ul>
             <?php foreach ($mano_atmintis as $value): ?>
+                <li>
+                    <?php print $value; ?>
+                </li>
+            <?php endforeach; ?>
+        </ul>
+        <h2>Draugo atmintis</h2>
+        <ul>
+            <?php foreach ($draugo_atmintis as $value): ?>
                 <li>
                     <?php print $value; ?>
                 </li>
