662adbda149f99528f48f292688eb6a9ad3257d1
mantasms <m.sakaitis@gmail.com>
2019-01-28 13:07:58 +0200

    foreach ciklas

--- index.php
+++ index.php
@@ -1,10 +1,46 @@
 <?php
-$mano_atmintis = ['Penktadienis', 'Paskaitos', 'Zmona', 'Parduotuve', 'Mokykla', 'Namai', 'Sriuba', 'Alus', 'Pica', 'Alus', 'Vynas', 'Kosmosas'];
-$draugo_atmintis = ['Penktadienis', 'Paskaitos', 'Alus', 'Alus', 'Degtine', 'Namai', 'Bliudas'];
+$mano_atmintis = [
+    'Penktadienis',
+    'Paskaitos',
+    'Zmona',
+    'Parduotuve',
+    'Mokykla',
+    'Namai',
+    'Sriuba',
+    'Alus',
+    'Pica',
+    'Alus',
+    'Vynas',
+    'Kosmosas'
+];
+
+$draugo_atmintis = [
+    'Penktadienis',
+    'Paskaitos',
+    'Alus',
+    'Alus',
+    'Degtine',
+    'Namai',
+    'Bliudas'
+];
 
 $count = count($mano_atmintis);
 $rand = rand(1, $count);
-$key = $rand - 1;
+$flashback_key = $rand - 1;
+$bendra_atmintis = [];
+//$count_draugo = 0;
+
+foreach ($draugo_atmintis as $value) {
+    if (!in_array($value, $bendra_atmintis)) {
+        if (in_array($value, $mano_atmintis)) {
+            array_push($bendra_atmintis, $value);
+        }
+    }
+}
+if (!in_array($mano_atmintis[$flashback_key], $bendra_atmintis)) {
+        array_push($bendra_atmintis, $mano_atmintis[$flashback_key]);
+}
+
 ?>
 
 <html>
@@ -14,7 +50,7 @@ $key = $rand - 1;
     <body>
         <h1>WTF?</h1>
         <h2>Mano atmintis</h2>
-        <h3>Flashback <?php print $rand . ': ' . $mano_atmintis[$key]; ?> </h3>
+        <h3>Flashback <?php print $rand . ': ' . $mano_atmintis[$flashback_key]; ?> </h3>
         <ul>
             <?php foreach ($mano_atmintis as $value): ?>
                 <li>
@@ -25,6 +61,14 @@ $key = $rand - 1;
         <h2>Draugo atmintis</h2>
         <ul>
             <?php foreach ($draugo_atmintis as $value): ?>
+                <li>
+                    <?php print $value; ?>
+                </li>
+            <?php endforeach; ?>
+        </ul>
+        <h2>Bendra atmintis 100%</h2>
+        <ul>
+            <?php foreach ($bendra_atmintis as $value): ?>
                 <li>
                     <?php print $value; ?>
                 </li>
