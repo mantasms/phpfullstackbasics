efb6ff96e97d5c06bd2ce19b2a9fb591e0e077e4
mantasms <m.sakaitis@gmail.com>
2019-01-30 12:25:09 +0200

    multidimension array sukurimas

--- index.php
+++ index.php
@@ -1,14 +1,55 @@
 <?php
-function add($x, $y){
-$suma = $x + $y;
-return $suma;
-}
+$auto = [
+    [
+        'gamintojas' => 'Fiat',
+        'modelis' => '126p',
+        'metai' => [
+            'from' => 1972,
+            'to' => 2000
+        ],
+        'gamyklos' =>
+        [
+            'Kasinas',
+            'Termini Imereze',
+            'Kragujevacas',
+            'Belsko-Biala',
+            'Tychos'
+        ],
+        'klase' => 'miesto automobilis',
+        'kebulo tipas' =>
+        [
+            '2 duru sedanas',
+            '2 duru hecbekas'
+        ],
+        'variklis' =>
+        [
+            'cilindrai' => 2,
+            'turis' =>
+            [
+                594,
+                652,
+                704
+            ]
+        ],
+        [
+            'dizaineris' => 'Sergio Sartorelli'
+        ]
+    ]
+];
+var_dump($auto);
 ?>
+
 <html>
     <head>
-        <title>Funkcijos pirma uzduotis</title>
+        <title>AUTO</title>
+        <link rel="stylesheet" type="text/css" href="/css/main.css">
     </head>
     <body>
-        <h1><?php print add(9, 6) ?></h2>
-</body>
+        <h1>Balius</h1>
+        <?php foreach ($balius as $dish): ?>
+            <p><?php print $dish['kiekis']; ?>
+                <span class="<?php print $dish['matas']; ?>"></span>
+            </p>
+        <?php endforeach; ?>
+    </body>
 </html>
