58ea41c2f2a496b9ff738c09c588febd3ba1e510
mantasms <m.sakaitis@gmail.com>
2019-02-05 12:02:58 +0200

    php references

--- index.php
+++ index.php
@@ -1,80 +1,11 @@
 <?php
-$bbd = [
-    [
-        'forma' => 'apvali',
-        'spalva' => 'zalia',
-        'textas' => 'AS'
-    ],
-    [
-        'forma' => 'kvadratas',
-        'spalva' => 'zalia',
-        'textas' => 'B'
-    ],
-    [
-        'forma' => 'kvadratas',
-        'spalva' => 'orange',
-        'textas' => 'B'
-    ],
-    [
-        'forma' => 'kvadratas',
-        'spalva' => 'raudona',
-        'textas' => 'D'
-    ]
-];
 
-$istorijos = [
-    'dienos pradzia',
-    'pirms leveel lorem ipsum blablabla',
-    'antras level pajabat',
-    'trecias level kurwa nx istorija',
-];
+$x = 0;
 
-function pzdamat($bbd, $level) {
-    foreach ($bbd as $key => &$stulpelis) {
-        $stulpelis['show_text'] = false;
-        if ($key > $level) {
-            $stulpelis['spalva'] = 'pilka';
-        }
-    }
-    $bbd[$level]['show_text'] = true;
-
-    return $bbd;
+function change_x(&$x) {
+    $x = 1;
 }
 
-$level = rand(0, count($bbd) - 1);
-print $level;
-$bbd = pzdamat($bbd, $level);
-var_dump($bbd);
-
-function istorijos($istorijos, $level) {
-    foreach ($istorijos as $key => $istorija) {
-        if ($key > $level) {
-            unset($istorijos[$key]);
-        }
-    }
-
-    return $istorijos;
-}
-
-$istorijos = istorijos($istorijos, $level);
-var_dump($istorijos);
-
+change_x($x);
+print $x;
 ?>
-<html>
-    <head>
-        <title>BBD MAT</title>
-        <link rel="stylesheet" type="text/css" href="/css/main.css">
-    </head>
-    <body>
-        <h1>BBD</h1>
-        <div class="flex-container">
-            <?php foreach ($bbd as $stulpelis): ?>
-                <div class='block <?php print $stulpelis['spalva'] . ' ' . $stulpelis['forma']; ?>'>
-                    <?php if ($stulpelis['show_text']): ?>
-                        <span><?php print $stulpelis['textas']; ?></span>
-                    <?php endif; ?>
-                </div>
-            <?php endforeach; ?>
-        </div>
-    </body>
-</html>
