bd28fb3e1908bba6e14593e0de17a9f2ca82b310
mantasms <m.sakaitis@gmail.com>
2019-02-05 13:17:35 +0200

    php references su avimis

--- index.php
+++ index.php
@@ -5,7 +5,13 @@ $sheep = ['bliea'];
 for ($i = 1; $i <= 4; $i++) {
     $sheep[] = &$sheep[$i - 1];
 }
+var_dump($sheep);
 
-$sheep[0] = 'kikiolki maklioski';
-var_dump($sheep)
+foreach ($sheep as $key => $value) {
+    unset($sheep[$key]);
+    $sheep[$key] = $value;
+}
+
+$sheep[3] = 'velniop sistema kikiolki maklioshki';
+var_dump($sheep);
 ?>
