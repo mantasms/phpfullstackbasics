931324af62945c9f26a29b8f12e88635ed773bcc
mantasms <m.sakaitis@gmail.com>
2019-02-05 12:14:54 +0200

    php references

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <?php
 
-$x = 0;
+$roll_joints = true;
+$joint1 = &$roll_joints;
+$joint2 = &$joint1;
+$joint3 = &$joint2;
 
-function change_x(&$x) {
-    $x = 1;
-}
-
-change_x($x);
-print $x;
+print $joint1;
+print $joint2;
+print $joint3;
 ?>
