15fbb0d81a9e7d7774292b2344003139230ba1b2
mantasms <m.sakaitis@gmail.com>
2019-02-06 12:28:10 +0200

    formos

--- index.php
+++ index.php
@@ -1,17 +1,21 @@
 <?php
-
-$sheep = ['bliea'];
-
-for ($i = 1; $i <= 4; $i++) {
-    $sheep[] = &$sheep[$i - 1];
-}
-var_dump($sheep);
-
-foreach ($sheep as $key => $value) {
-    unset($sheep[$key]);
-    $sheep[$key] = $value;
+if (isset($_POST['skaicius'])) {
+    $skaicius = $_POST['skaicius'];
 }
 
-$sheep[3] = 'velniop sistema kikiolki maklioshki';
-var_dump($sheep);
+function square($skaicius) {
+    return $skaicius ** 2;
+}
 ?>
+<html>
+    <head>
+        <title>formos</title>
+    </head>
+    <body>
+        <form action="index.php" method="POST">
+            Ka pakelti kvadratu: <input name="skaicius" type="number" placeholder="Iveskite skaiciu"/>
+            <input type="submit"/>
+        </form>
+        <h1>Atsakymas: <?php print square($skaicius); ?></h1>
+    </body>
+</html>
