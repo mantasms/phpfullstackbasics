487d451d964e6941cc6472b3c6cc0318cde0407b
mantasms <m.sakaitis@gmail.com>
2019-02-06 12:48:47 +0200

    formos

--- index.php
+++ index.php
@@ -1,11 +1,20 @@
 <?php
+$atsakymas = 0;
+
 if (isset($_POST['skaicius'])) {
     $skaicius = $_POST['skaicius'];
+    if (empty($skaicius)) {
+        $atsakymas = 'Prasau ivesti skaiciu!';
+    } else {
+        $square = square($skaicius);
+        $atsakymas = "$skaicius pakeltas kvadratu yra: $square";
+    }
 }
 
 function square($skaicius) {
     return $skaicius ** 2;
 }
+
 ?>
 <html>
     <head>
@@ -16,6 +25,6 @@ function square($skaicius) {
             Ka pakelti kvadratu: <input name="skaicius" type="number" placeholder="Iveskite skaiciu"/>
             <input type="submit"/>
         </form>
-        <h1>Atsakymas: <?php print square($skaicius); ?></h1>
+        <h1><?php print $atsakymas; ?></h1>
     </body>
 </html>
