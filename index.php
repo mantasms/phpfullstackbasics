835b926ee2f822606377c0a7c9c53baa601acfbb
mantasms <m.sakaitis@gmail.com>
2019-02-06 13:47:03 +0200

    veiksmai su formomis skaiciavimai ir spausdinimas pagal clickus

--- index.php
+++ index.php
@@ -1,34 +1,9 @@
 <?php
-$atsakymas = null;
+$clickai = 0;
 
-if (isset($_POST['skaicius'])) {
-    $skaicius = $_POST['skaicius'];
-    if (empty($skaicius)) {
-        $atsakymas = 'Prasau ivesti skaiciu!';
-    } else {
-        $square = square($skaicius);
-        $atsakymas = "$skaicius pakeltas kvadratu yra: $square";
-    }
+if (isset($_POST['click'])) {
+    $clickai = $_POST['click'] + 1;
 }
-
-function square($skaicius) {
-    return $skaicius ** 2;
-}
-
-// Dainiaus optimalus uzduoties kodas:
-// 
-//function square($x) {
-//    return $x ** 2;
-//}
-//
-//$atsakymas = 'Ivesk duomenis';
-//		
-//$skaicius = $_POST['skaicius'] ?? false;
-//
-//if (!empty($skaicius)) {
-//    $atsakymas = square($skaicius);
-//}
-
 ?>
 <html>
     <head>
@@ -36,9 +11,10 @@ function square($skaicius) {
     </head>
     <body>
         <form action="index.php" method="POST">
-            Ka pakelti kvadratu: <input name="skaicius" type="number" placeholder="Iveskite skaiciu"/>
-            <input type="submit"/>
+            <button name="click" value="<?php print $clickai; ?>"><?php print $clickai; ?></button>
         </form>
-        <h1><?php print $atsakymas; ?></h1>
+                <?php for ($i = 0; $i < $clickai; $i++): ?>
+            <img width=400 src="images/sunny.jpg">
+        <?php endfor; ?>
     </body>
 </html>
