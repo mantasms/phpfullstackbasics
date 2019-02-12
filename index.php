335ef57c74c74976e2644bac75e375a875fd9b0a
mantasms <m.sakaitis@gmail.com>
2019-02-12 11:08:41 +0200

    saugios formos kurimas Dainiaus cleancode

--- index.php
+++ index.php
@@ -1,66 +1,97 @@
 <?php
+
+/**
+ * Prevents HTML/JS/MYSQL injection
+ * for all $form fields
+ * & adds error messages if so
+ * @param array $form
+ * @return array Safe Input
+ */
+function get_safe_input($form) {
+    $filtro_parametrai = [
+        'action' => FILTER_SANITIZE_SPECIAL_CHARS
+    ];
+
+    foreach ($form['fields'] as $field_id => $value) {
+        $filtro_parametrai[$field_id] = FILTER_SANITIZE_SPECIAL_CHARS;
+    }
+
+    return filter_input_array(INPUT_POST, $filtro_parametrai);
+}
+
+/**
+ * Check all form fields if they are not empty
+ * & adds error messages if so
+ * @param array $safe_input
+ * @param array $form
+ * @return type
+ */
+function validate_not_empty($safe_input, &$form) {
+    foreach ($form['fields'] as $field_id => &$field) {
+        if ($safe_input[$field_id] == '') {
+            $field['error_msg'] = strtr('Jobans/a tu buhurs/gazele, '
+                    . 'kad palikai @field tuscia!', ['@field' => $field['label']
+            ]);
+        }
+    }
+}
+
 $form = [
-    'input' => [
-        'name' => [
-            'text' => 'Mano vardas',
+    'fields' => [
+        'vardas' => [
+            'label' => 'Mano vardas',
             'type' => 'text',
-            'placeholder' => 'Vardas',
+            'placeholder' => 'Vardas'
         ],
-        'zirniai' => [
-            'text' => 'Kiek turiu zirniu ?',
+        'zirniu_kiekis' => [
+            'label' => 'Kiek turiu zirniu?',
             'type' => 'text',
-            'placeholder' => '1-100',
+            'placeholder' => '1-100'
         ],
-        'reason' => [
-            'text' => 'Paslaptis, kodel turiu zirniu',
+        'paslaptis' => [
+            'label' => 'Paslaptis, kodel turiu zirniu',
             'type' => 'password',
-            'placeholder' => 'Issipasakok',
+            'placeholder' => 'Issipasakok'
         ]
     ],
-    'button' => [
+    'buttons' => [
         'do_zirniai' => [
-            'text' => 'Paberti...',
+            'text' => 'Paberti...'
         ]
     ]
 ];
 
-function get_safe_input($form) {
-    $filter_param = [];
-    foreach ($form['input'] as $key => $value) {
-        $filter_param[$key] = FILTER_SANITIZE_SPECIAL_CHARS;
-    }
-    $filter_param['action'] = FILTER_SANITIZE_SPECIAL_CHARS;
-    return filter_input_array(INPUT_POST, $filter_param);
+if (!empty($_POST)) {
+    $safe_input = get_safe_input($form);
+    validate_not_empty($safe_input, $form);
 }
-
-var_dump(get_safe_input($form));
-
-// kodas paimtas is Donatas Jurkus repositorium
-// 
-//$input = function get_safe_input($forma);
-//
-//function validate_not_empty($input, &$forma) {
-//    foreach ($form ['input'] as $fields)
-//}
 ?>
+
 <html>
     <head>
-        <title>Hack Me</title>
-        <link rel="stylesheet" href="css/style.css"/>
+        <title>02/11/2019</title>
+        <link rel="stylesheet" href="css/style.css">
     </head>
     <body>
-        <h1></h1>
-        <h2>Hack this page</h2>
+        <h1>Generuojam forma is array</h1>
         <form method="POST">
-<?php foreach ($form['input'] as $input_key => $input): ?>
+            <!-- Input Fields -->
+            <?php foreach ($form['fields'] as $field_id => $field): ?>
                 <label>
-                    <span><?php print $input['text']; ?></span>
-                    <input type="<?php print $input['type']; ?>" name="<?php print $input_key; ?>" placeholder="<?php print $input['placeholder']; ?>">
+                    <span><?php print $field['label']; ?></span>
+                    <input type="<?php print $field['type']; ?>" name="<?php print $field_id; ?>" placeholder="<?php print $field['placeholder']; ?>"/>
+                    <?php if (isset($field['error_msg'])): ?>
+                        <span class="error"><?php print $field['error_msg']; ?></span>
+                    <?php endif; ?>
                 </label>
             <?php endforeach; ?>
-            <?php foreach ($form['button'] as $button_key => $button): ?>
-                <button name="action" value="<?php print $button_key; ?>"><?php print $button['text']; ?></button>
-<?php endforeach; ?>
+
+            <!-- Buttons -->
+            <?php foreach ($form['buttons'] as $button_id => $button): ?>
+                <button name="action" value="<?php print $button_id; ?>">
+                    <?php print $button['text']; ?>
+                </button>
+            <?php endforeach; ?>
         </form>
     </body>
 </html>
